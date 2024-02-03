36d7d155af62b96684ed02ae663d84a0936d4870
Muhammad Shakeel ur Rehman Tariq <70shakeel@gmail.com>
2024-02-03 12:13:27 +0500

    kids with greatest number of candies

--- main.js
+++ main.js
@@ -1,43 +1,52 @@
-// Greatest Common Divisor of Strings
+// Kids With the Greatest Number of Candies
 // Easy
 // Topics
 // Companies
 // Hint
-// For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+// There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
 
-// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+// Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+// Note that multiple kids can have the greatest number of candies.
 
 
 
 // Example 1:
 
-// Input: str1 = "ABCABC", str2 = "ABC"
-// Output: "ABC"
+// Input: candies = [2,3,5,1,3], extraCandies = 3
+// Output: [true,true,true,false,true] 
+// Explanation: If you give all extraCandies to:
+// - Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+// - Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+// - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+// - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+// - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
 // Example 2:
 
-// Input: str1 = "ABABAB", str2 = "ABAB"
-// Output: "AB"
+// Input: candies = [4,2,1,1,2], extraCandies = 1
+// Output: [true,false,false,false,false] 
+// Explanation: There is only 1 extra candy.
+// Kid 1 will always have the greatest number of candies, even if a different kid is given the extra candy.
 // Example 3:
 
-// Input: str1 = "LEET", str2 = "CODE"
-// Output: ""
-
-
-// Constraints:
-
-// 1 <= str1.length, str2.length <= 1000
-// str1 and str2 consist of English uppercase letters.
+// Input: candies = [12,1,12], extraCandies = 10
+// Output: [true,false,true]
 /**
- * @param {string} str1
- * @param {string} str2
- * @return {string}
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
  */
-var gcdOfStrings = function (str1, str2) {
-    if (str1 + str2 !== str2 + str1) {
-        return ""; // If the strings don't have a common prefix, return an empty string
+var kidsWithCandies = function (candies, extraCandies) {
+    const max = Math.max(...candies);
+    let resultArr = []
+    for (let i = 0; i < candies.length; i++) {
+        if ((candies[i] + extraCandies) > max || (candies[i] + extraCandies) == max) {
+            resultArr.push(true);
+        }
+        else {
+            resultArr.push(false)
+        }
     }
+    return resultArr;
 
-    // Find the GCD using Euclidean algorithm
-    const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
-    return str1.substring(0, gcd(str1.length, str2.length));
 };
