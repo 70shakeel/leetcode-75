2f8d79624ee8992a5349c91608352a667e3d899c
Muhammad Shakeel ur Rehman Tariq <70shakeel@gmail.com>
2024-02-07 13:51:05 +0500

    reverse words in a string

--- main.js
+++ main.js
@@ -1,63 +1,40 @@
-// Reverse Vowels of a String
-// Easy
+// Reverse Words in a String
+// Medium
 // Topics
 // Companies
-// Given a string s, reverse only all the vowels in the string and return it.
+// Given an input string s, reverse the order of the words.
 
-// The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+// A word is defined as a sequence of non - space characters.The words in s will be separated by at least one space.
+
+// Return a string of the words in reverse order concatenated by a single space.
+
+// Note that s may contain leading or trailing spaces or multiple spaces between two words.The returned string should only have a single space separating the words.Do not include any extra spaces.
 
 
 
 //     Example 1:
 
-// Input: s = "hello"
-// Output: "holle"
+// Input: s = "the sky is blue"
+// Output: "blue is sky the"
 // Example 2:
 
-// Input: s = "leetcode"
-// Output: "leotcede"
+// Input: s = "  hello world  "
+// Output: "world hello"
+// Explanation: Your reversed string should not contain leading or trailing spaces.
+//     Example 3:
+
+// Input: s = "a good   example"
+// Output: "example good a"
+// Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
 /**
  * @param {string} s
  * @return {string}
  */
-var reverseVowels = function (s) {
-    function removeVowelsFromString(str) {
-        return str.replace(/[aeiouAEIOU]/g, '');
-    }
-    function addLetterToString(str, index, letter) {
-        if (index < 0 || index > str.length) {
-            return "Invalid index";
-        }
-        return str.slice(0, index) + letter + str.slice(index);
-    }
-    function isVowel(character) {
-        // Convert the character to lowercase to handle uppercase vowels as well
-        character = character.toLowerCase();
+var reverseWords = function (s) {
+    let removedSpaces = s.replace(/\s+/g, ' ').trim();
+    let words = removedSpaces.split(" ");
+    let reverseWords = words.reverse();
+    let result = reverseWords.join(" ");
+    return result;
 
-        // Check if the character is one of the vowels
-        return ['a', 'e', 'i', 'o', 'u'].includes(character);
-    }
-    let indexArr = [];
-    let vowelArr = [];
-    let reverseVowelArr = [];
-    let modifiedString = "";
-    for (let i = 0; i < s.length; i++) {
-
-        if (isVowel(s[i])) {
-            indexArr.push(i);
-            vowelArr.push(s[i]);
-        }
-
-    }
-    modifiedString = removeVowelsFromString(s);
-    console.log("vowel array", vowelArr);
-
-    reverseVowelArr = vowelArr.reverse();
-    for (let i = 0; i < vowelArr.length; i++) {
-        modifiedString = addLetterToString(modifiedString, indexArr[i], reverseVowelArr[i])
-    }
-    console.log("reverseVowelArr: ", reverseVowelArr);
-    console.log("index array", indexArr);
-    console.log("modified string", modifiedString)
-    return modifiedString;
 };
