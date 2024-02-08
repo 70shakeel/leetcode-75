0f82c5f4f24b511f654b571b7758914619ef892e
Muhammad Shakeel ur Rehman Tariq <70shakeel@gmail.com>
2024-02-08 14:36:05 +0500

    product of array except self

--- main.js
+++ main.js
@@ -1,40 +1,51 @@
-// Reverse Words in a String
+// Product of Array Except Self
+// Solved
 // Medium
 // Topics
 // Companies
-// Given an input string s, reverse the order of the words.
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 
-// A word is defined as a sequence of non - space characters.The words in s will be separated by at least one space.
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32 - bit integer.
 
-// Return a string of the words in reverse order concatenated by a single space.
-
-// Note that s may contain leading or trailing spaces or multiple spaces between two words.The returned string should only have a single space separating the words.Do not include any extra spaces.
+// You must write an algorithm that runs in O(n) time and without using the division operation.
 
 
 
 //     Example 1:
 
-// Input: s = "the sky is blue"
-// Output: "blue is sky the"
+// Input: nums = [1, 2, 3, 4]
+// Output: [24, 12, 8, 6]
 // Example 2:
 
-// Input: s = "  hello world  "
-// Output: "world hello"
-// Explanation: Your reversed string should not contain leading or trailing spaces.
-//     Example 3:
-
-// Input: s = "a good   example"
-// Output: "example good a"
-// Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+// Input: nums = [-1, 1, 0, -3, 3]
+// Output: [0, 0, 9, 0, 0]
 /**
- * @param {string} s
- * @return {string}
+ * @param {number[]} nums
+ * @return {number[]}
  */
-var reverseWords = function (s) {
-    let removedSpaces = s.replace(/\s+/g, ' ').trim();
-    let words = removedSpaces.split(" ");
-    let reverseWords = words.reverse();
-    let result = reverseWords.join(" ");
-    return result;
+var productExceptSelf = function (nums) {
+    let product = 1;
+    let zeroCount = 0;
 
+    // Calculate the product of all elements and count zeroes
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            product *= nums[i];
+        } else {
+            zeroCount++;
+        }
+    }
+
+    // If there are more than one zero, all elements except zeroes will be 0
+    if (zeroCount > 1) {
+        return Array(nums.length).fill(0);
+    }
+
+    // If there is only one zero, all elements except zero will be 0, and zero will be the product of all other elements
+    if (zeroCount === 1) {
+        return nums.map(num => num === 0 ? product : 0);
+    }
+
+    // If there are no zeroes, divide the product by the current element to get the product except self
+    return nums.map(num => product / num);
 };
