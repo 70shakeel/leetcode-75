e8e8608deb5c848e00112f8dea88721129f95db2
Muhammad Shakeel ur Rehman Tariq <70shakeel@gmail.com>
2024-02-13 13:00:47 +0500

    moving zeros to end of array

--- main.js
+++ main.js
@@ -1,68 +1,40 @@
-// String Compression
+// Move Zeroes
 // Solved
-// Medium
+// Easy
 // Topics
 // Companies
 // Hint
-// Given an array of characters chars, compress it using the following algorithm:
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
-// Begin with an empty string s. For each group of consecutive repeating characters in chars:
-
-// If the group's length is 1, append the character to s.
-// Otherwise, append the character followed by the group's length.
-// The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
-
-// After you are done modifying the input array, return the new length of the array.
-
-// You must write an algorithm that uses only constant extra space.
+// Note that you must do this in -place without making a copy of the array.
 
 
 
-// Example 1:
+//     Example 1:
 
-// Input: chars = ["a","a","b","b","c","c","c"]
-// Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
-// Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+// Input: nums = [0, 1, 0, 3, 12]
+// Output: [1, 3, 12, 0, 0]
 // Example 2:
 
-// Input: chars = ["a"]
-// Output: Return 1, and the first character of the input array should be: ["a"]
-// Explanation: The only group is "a", which remains uncompressed since it's a single character.
-// Example 3:
-
-// Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
-// Output: Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
-// Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
+// Input: nums = [0]
+// Output: [0]
 /**
- * @param {character[]} chars
- * @return {number}
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
  */
-var compress = function (chars) {
-    if (chars.length <= 1) {
-        return chars.length;
-    }
-
-    let currentIndex = 0;
-    let count = 1;
-
-    for (let i = 1; i <= chars.length; i++) {
-        if (i < chars.length && chars[i] === chars[i - 1]) {
-            count++;
-        } else {
-            chars[currentIndex] = chars[i - 1];
-            currentIndex++;
-
-            if (count > 1) {
-                const countStr = count.toString();
-                for (let j = 0; j < countStr.length; j++) {
-                    chars[currentIndex] = countStr[j];
-                    currentIndex++;
-                }
-            }
-            count = 1;
+var moveZeroes = function (nums) {
+    let numberOfZeros = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            console.log("zero #", i);
+            nums.splice(i, 1);
+            i--;
+            numberOfZeros++;
         }
     }
-    chars.splice(currentIndex);
-
-    return chars.length;
+    console.log("zeros: ", numberOfZeros);
+    for (let i = 0; i < numberOfZeros; i++) {
+        nums.push(0);
+        console.log("nums arr: ", nums)
+    }
 };
