92b2cb6bf47637db481270d10c1eff7bb506bbab
Muhammad Shakeel ur Rehman Tariq <70shakeel@gmail.com>
2024-02-14 12:12:40 +0500

    if a substring is a subsequence of a string like abc in aabxyzc

--- main.js
+++ main.js
@@ -1,40 +1,54 @@
-// Move Zeroes
+// Is Subsequence
 // Solved
 // Easy
 // Topics
 // Companies
-// Hint
-// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
-// Note that you must do this in -place without making a copy of the array.
-
+// A subsequence of a string is a new string that is formed from the original string by deleting some(can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
 
-//     Example 1:
 
-// Input: nums = [0, 1, 0, 3, 12]
-// Output: [1, 3, 12, 0, 0]
+// Example 1:
+
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
 // Example 2:
 
-// Input: nums = [0]
-// Output: [0]
+// Input: s = "axc", t = "ahbgdc"
+// Output: false
 /**
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
  */
-var moveZeroes = function (nums) {
-    let numberOfZeros = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 0) {
-            console.log("zero #", i);
-            nums.splice(i, 1);
-            i--;
-            numberOfZeros++;
+var isSubsequence = function (s, t) {
+    let result = [];
+    let len = t.length;
+    for (let i = 0; i < s.length; i++) {
+        if (t.includes(s[i])) {
+            let index = t.indexOf(s[i]);
+            let newStr = t.substring(index + 1);
+            // let regex = new RegExp(t[i], "g");
+            // let newS=s.replace(regex,"");
+
+            t = newStr;
+            // s=newS;            
+            console.log("s:", s);
+            console.log("t :", t);
+            // console.log("length: ",len)
+            result.push(true);
+
+        }
+        else {
+            result.push(false)
         }
     }
-    console.log("zeros: ", numberOfZeros);
-    for (let i = 0; i < numberOfZeros; i++) {
-        nums.push(0);
-        console.log("nums arr: ", nums)
+    if (result.includes(false)) {
+        return false;
+    }
+    else {
+        return true;
+
     }
 };
