347680e9cd84a585155d2fa0b55cd95fcafc6d5f
Muhammad Shakeel ur Rehman Tariq <70shakeel@gmail.com>
2024-02-15 12:48:01 +0500

    container with most water, lines having max area between them

--- main.js
+++ main.js
@@ -1,54 +1,49 @@
-// Is Subsequence
+// Container With Most Water
 // Solved
-// Easy
+// Medium
 // Topics
 // Companies
-// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+// Hint
+// You are given an integer array height of length n.There are n vertical lines drawn such that the two endpoints of the ith line are(i, 0) and(i, height[i]).
 
-// A subsequence of a string is a new string that is formed from the original string by deleting some(can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+// Find two lines that together with the x - axis form a container, such that the container contains the most water.
+
+// Return the maximum amount of water a container can store.
+
+// Notice that you may not slant the container.
 
 
 
-// Example 1:
+//     Example 1:
 
-// Input: s = "abc", t = "ahbgdc"
-// Output: true
+
+// Input: height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+// Output: 49
+// Explanation: The above vertical lines are represented by array[1, 8, 6, 2, 5, 4, 8, 3, 7].In this case, the max area of water(blue section) the container can contain is 49.
 // Example 2:
 
-// Input: s = "axc", t = "ahbgdc"
-// Output: false
+// Input: height = [1, 1]
+// Output: 1
 /**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
+ * @param {number[]} height
+ * @return {number}
  */
-var isSubsequence = function (s, t) {
-    let result = [];
-    let len = t.length;
-    for (let i = 0; i < s.length; i++) {
-        if (t.includes(s[i])) {
-            let index = t.indexOf(s[i]);
-            let newStr = t.substring(index + 1);
-            // let regex = new RegExp(t[i], "g");
-            // let newS=s.replace(regex,"");
+var maxArea = function (height) {
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length - 1;
 
-            t = newStr;
-            // s=newS;            
-            console.log("s:", s);
-            console.log("t :", t);
-            // console.log("length: ",len)
-            result.push(true);
+    while (left < right) {
+        let h = Math.min(height[left], height[right]);
+        let area = h * (right - left);
+        maxArea = Math.max(maxArea, area);
 
-        }
-        else {
-            result.push(false)
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
         }
     }
-    if (result.includes(false)) {
-        return false;
-    }
-    else {
-        return true;
 
-    }
+    return maxArea;
 };
