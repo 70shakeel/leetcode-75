c1f2e9eae39c98618ccf25399520561c2df575c3
Muhammad Shakeel ur Rehman Tariq <70shakeel@gmail.com>
2024-02-17 12:57:03 +0500

    maximum average subarray

--- main.js
+++ main.js
@@ -1,55 +1,34 @@
-// Max Number of K - Sum Pairs
+// Maximum Average Subarray I
 // Solved
-// Medium
+// Easy
 // Topics
 // Companies
-// Hint
-// You are given an integer array nums and an integer k.
+// You are given an integer array nums consisting of n elements, and an integer k.
 
-// In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
-
-// Return the maximum number of operations you can perform on the array.
+// Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value.Any answer with a calculation error less than 10 - 5 will be accepted.
 
 
 
 //     Example 1:
 
-// Input: nums = [1, 2, 3, 4], k = 5
-// Output: 2
-// Explanation: Starting with nums = [1, 2, 3, 4]:
-// - Remove numbers 1 and 4, then nums = [2, 3]
-//     - Remove numbers 2 and 3, then nums = []
-// There are no more pairs that sum up to 5, hence a total of 2 operations.
-//     Example 2:
+// Input: nums = [1, 12, -5, -6, 50, 3], k = 4
+// Output: 12.75000
+// Explanation: Maximum average is(12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+// Example 2:
 
-// Input: nums = [3, 1, 3, 4, 3], k = 6
-// Output: 1
-// Explanation: Starting with nums = [3, 1, 3, 4, 3]:
-// - Remove the first two 3's, then nums = [1,4,3]
-// There are no more pairs that sum up to 6, hence a total of 1 operation.
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
-var maxOperations = function (nums, k) {
-    nums.sort((a, b) => a - b);
-    let left = 0;
-    let right = nums.length - 1;
-    let result = 0;
-
-    while (left < right) {
-        let sum = nums[left] + nums[right];
-        if (sum === k) {
-            result++;
-            left++;
-            right--;
-        } else if (sum < k) {
-            left++;
-        } else {
-            right--;
-        }
+// Input: nums = [5], k = 1
+// Output: 5.00000
+var findMaxAverage = function (nums, k) {
+    let sum = 0;
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
     }
 
-    return result;
+    let maxSum = sum;
+    for (let i = k; i < nums.length; i++) {
+        sum += nums[i] - nums[i - k];
+        maxSum = Math.max(maxSum, sum);
+    }
+
+    return maxSum / k;
 };
