068e7b1e578e7c0f4d4a818c2b1a73b8edea3b67
Macbook <macbook@Shakeels-MacBook-Pro.local>
2024-03-02 12:35:35 +0500

    Dota 2 Senate

--- main.js
+++ main.js
@@ -1,41 +1,65 @@
-// Number of Recent Calls
-// Easy
+// Dota2 Senate
+// Solved
+// Medium
 // Topics
 // Companies
-// You have a RecentCounter class which counts the number of recent requests within a certain time frame.
+// In the world of Dota2, there are two parties: the Radiant and the Dire.
 
-// Implement the RecentCounter class:
+// The Dota2 senate consists of senators coming from two parties.Now the Senate wants to decide on a change in the Dota2 game.The voting for this change is a round - based procedure.In each round, each senator can exercise one of the two rights:
 
-// RecentCounter() Initializes the counter with zero recent requests.
-// int ping(int t) Adds a new request at time t, where t represents some time in milliseconds, and returns the number of requests that has happened in the past 3000 milliseconds(including the new request).Specifically, return the number of requests that have happened in the inclusive range[t - 3000, t].
-// It is guaranteed that every call to ping uses a strictly larger value of t than the previous call.
+// Ban one senator's right: A senator can make another senator lose all his rights in this and all the following rounds.
+// Announce the victory: If this senator found the senators who still have rights to vote are all from the same party, he can announce the victory and decide on the change in the game.
+// Given a string senate representing each senator's party belonging. The character 'R' and 'D' represent the Radiant party and the Dire party. Then if there are n senators, the size of the given string will be n.
+
+// The round - based procedure starts from the first senator to the last senator in the given order.This procedure will last until the end of voting.All the senators who have lost their rights will be skipped during the procedure.
+
+// Suppose every senator is smart enough and will play the best strategy for his own party.Predict which party will finally announce the victory and change the Dota2 game.The output should be "Radiant" or "Dire".
 
 
 
 //     Example 1:
 
-// Input
-// ["RecentCounter", "ping", "ping", "ping", "ping"]
-// [[], [1], [100], [3001], [3002]]
-// Output
-// [null, 1, 2, 3, 3]
+// Input: senate = "RD"
+// Output: "Radiant"
+// Explanation: 
+// The first senator comes from Radiant and he can just ban the next senator's right in round 1. 
+// And the second senator can't exercise any rights anymore since his right has been banned. 
+// And in round 2, the first senator can just announce the victory since he is the only guy in the senate who can vote.
+//     Example 2:
 
-// Explanation
-// RecentCounter recentCounter = new RecentCounter();
-// recentCounter.ping(1);     // requests = [1], range is [-2999,1], return 1
-// recentCounter.ping(100);   // requests = [1, 100], range is [-2900,100], return 2
-// recentCounter.ping(3001);  // requests = [1, 100, 3001], range is [1,3001], return 3
-// recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002], range is [2,3002], return 3
-class RecentCounter {
-    constructor() {
-        this.requests = [];
-    }
+// Input: senate = "RDD"
+// Output: "Dire"
+// Explanation: 
+// The first senator comes from Radiant and he can just ban the next senator's right in round 1. 
+// And the second senator can't exercise any rights anymore since his right has been banned. 
+// And the third senator comes from Dire and he can ban the first senator's right in round 1. 
+// And in round 2, the third senator can just announce the victory since he is the only guy in the senate who can vote.
+/**
+ * @param {string} senate
+ * @return {string}
+ */
+var predictPartyVictory = function (senate) {
+    let radiant = [];
+    let dire = [];
+    const n = senate.length;
 
-    ping(t) {
-        this.requests.push(t);
-        while (this.requests[0] < t - 3000) {
-            this.requests.shift();
+    for (let i = 0; i < n; i++) {
+        if (senate[i] === 'R') {
+            radiant.push(i);
+        } else {
+            dire.push(i);
         }
-        return this.requests.length;
     }
+
+    while (radiant.length > 0 && dire.length > 0) {
+        if (radiant[0] < dire[0]) {
+            radiant.push(radiant[0] + n);
+        } else {
+            dire.push(dire[0] + n);
+        }
+        radiant.shift();
+        dire.shift();
+    }
+
+    return radiant.length > 0 ? 'Radiant' : 'Dire';
 }
