fbccf25a0689d9eb87d16382fde3795eac070ad3
Shakeel <70shakeel@gmail.com>
2024-03-13 11:51:15 +0500

    binary tree right side view

--- main.js
+++ main.js
@@ -1,30 +1,24 @@
-// Lowest Common Ancestor of a Binary Tree
+// Binary Tree Right Side View
 // Medium
 // Topics
 // Companies
-// Given a binary tree, find the lowest common ancestor(LCA) of two given nodes in the tree.
-
-// According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+// Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
 
 
 
-// Example 1:
+//     Example 1:
 
 
-// Input: root = [3, 5, 1, 6, 2, 0, 8, null, null, 7, 4], p = 5, q = 1
-// Output: 3
-// Explanation: The LCA of nodes 5 and 1 is 3.
+// Input: root = [1, 2, 3, null, 5, null, 4]
+// Output: [1, 3, 4]
 // Example 2:
 
+// Input: root = [1, null, 3]
+// Output: [1, 3]
+// Example 3:
 
-// Input: root = [3, 5, 1, 6, 2, 0, 8, null, null, 7, 4], p = 5, q = 4
-// Output: 5
-// Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
-//     Example 3:
-
-// Input: root = [1, 2], p = 1, q = 2
-// Output: 1
-
+// Input: root = []
+// Output: []
 class TreeNode {
     constructor(val = 0, left = null, right = null) {
         this.val = val;
@@ -33,12 +27,26 @@ class TreeNode {
     }
 }
 
-const lowestCommonAncestor = (root, p, q) => {
-    if (!root || root === p || root === q) return root;
+var rightSideView = function (root) {
+    if (!root) return [];
 
-    const left = lowestCommonAncestor(root.left, p, q);
-    const right = lowestCommonAncestor(root.right, p, q);
+    const result = [];
+    const queue = [root];
 
-    if (left && right) return root;
-    return left || right;
+    while (queue.length > 0) {
+        const levelSize = queue.length;
+        let lastNodeValue = null;
+
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift();
+            lastNodeValue = node.val;
+
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+        result.push(lastNodeValue);
+    }
+
+    return result;
 };
