cc295e0a1f927f3ec026fc18f3ad84c27aea49d2
Shakeel <70shakeel@gmail.com>
2024-03-26 15:44:13 +0500

    Binary Search: Guess Number Higher or Lower

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
-// Smallest Number in Infinite Set
-class SmallestInfiniteSet {
-    constructor() {
-        this.pq = new MinPriorityQueue();
-        this.s = new Set();
-        for (let i = 1; i <= 1000; i++) {
-            this.pq.enqueue(i, i);
-            this.s.add(i);
+// Guess Number Higher or Lower
+// Easy
+// Topics
+// Companies
+// We are playing the Guess Game.The game is as follows:
+
+// I pick a number from 1 to n.You have to guess which number I picked.
+
+// Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+// You call a pre - defined API int guess(int num), which returns three possible results:
+
+// -1: Your guess is higher than the number I picked(i.e.num > pick).
+// 1: Your guess is lower than the number I picked(i.e.num < pick).
+// 0: your guess is equal to the number I picked(i.e.num == pick).
+// Return the number that I picked.
+
+
+
+//     Example 1:
+
+// Input: n = 10, pick = 6
+// Output: 6
+// Example 2:
+
+// Input: n = 1, pick = 1
+// Output: 1
+// Example 3:
+
+// Input: n = 2, pick = 1
+// Output: 1
+function guessNumber(n) {
+    let left = 1;
+    let right = n;
+    while (left <= right) {
+        let mid = Math.floor(left + (right - left) / 2);
+        let result = guess(mid);
+        if (result === 0) {
+            return mid;
+        } else if (result === 1) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
-
-    popSmallest() {
-        const x = this.pq.dequeue()?.element;
-        this.s.delete(x);
-        return x;
-    }
-
-    addBack(num) {
-        if (!this.s.has(num)) {
-            this.pq.enqueue(num, num);
-            this.s.add(num);
-        }
-    }
+    return -1; // Not found
 }
