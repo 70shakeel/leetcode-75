8ef790be8a75b5535c926c923036ec9dca2d5638
Shakeel <70shakeel@gmail.com>
2024-03-31 14:52:44 +0500

    Backtracking: Combination Sum 3

--- main.js
+++ main.js
@@ -1,54 +1,54 @@
-// Letter Combinations of a Phone Number
+// Combination Sum III
 // Medium
 // Topics
 // Companies
-// Given a string containing digits from 2 - 9 inclusive, return all possible letter combinations that the number could represent.Return the answer in any order.
+// Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
 
-// A mapping of digits to letters(just like on the telephone buttons) is given below.Note that 1 does not map to any letters.
-
+// Only numbers 1 through 9 are used.
+// Each number is used at most once.
+// Return a list of all possible valid combinations.The list must not contain the same combination twice, and the combinations may be returned in any order.
 
 
 
 //     Example 1:
 
-// Input: digits = "23"
-// Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
-// Example 2:
+// Input: k = 3, n = 7
+// Output: [[1, 2, 4]]
+// Explanation:
+// 1 + 2 + 4 = 7
+// There are no other valid combinations.
+//     Example 2:
 
-// Input: digits = ""
+// Input: k = 3, n = 9
+// Output: [[1, 2, 6], [1, 3, 5], [2, 3, 4]]
+// Explanation:
+// 1 + 2 + 6 = 9
+// 1 + 3 + 5 = 9
+// 2 + 3 + 4 = 9
+// There are no other valid combinations.
+//     Example 3:
+
+// Input: k = 4, n = 1
 // Output: []
-// Example 3:
-
-// Input: digits = "2"
-// Output: ["a", "b", "c"]
-const letterCombinations = function (digits) {
-    if (!digits) return [];
-
-    const digitToLetters = {
-        '2': 'abc',
-        '3': 'def',
-        '4': 'ghi',
-        '5': 'jkl',
-        '6': 'mno',
-        '7': 'pqrs',
-        '8': 'tuv',
-        '9': 'wxyz'
-    };
-
+// Explanation: There are no valid combinations.
+//     Using 4 different numbers in the range[1, 9], the smallest sum we can get is 1 + 2 + 3 + 4 = 10 and since 10 > 1, there are no valid combination.
+const combinationSum3 = function (k, n) {
     const result = [];
-    const backtrack = (index, path) => {
-        if (index === digits.length) {
-            result.push(path);
+    const backtrack = (start, k, n, path) => {
+        if (k === 0 && n === 0) {
+            result.push([...path]);
             return;
         }
-
-        const letters = digitToLetters[digits[index]];
-        for (let letter of letters) {
-            backtrack(index + 1, path + letter);
+        if (k === 0 || n === 0) {
+            return;
+        }
+        for (let i = start; i <= 9; i++) {
+            path.push(i);
+            backtrack(i + 1, k - 1, n - i, path);
+            path.pop();
         }
     };
 
-    backtrack(0, '');
-
+    backtrack(1, k, n, []);
     return result;
 };
