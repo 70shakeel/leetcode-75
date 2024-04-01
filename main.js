429ed26d323ca9257c0895289f88afafba7627bb
Shakeel <70shakeel@gmail.com>
2024-04-01 14:17:10 +0500

    Dynamic Programming: Nth Tribonacci Number

--- main.js
+++ main.js
@@ -1,54 +1,39 @@
-// Combination Sum III
-// Medium
+// N - th Tribonacci Number
+// Easy
 // Topics
 // Companies
-// Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
+// Hint
+// The Tribonacci sequence Tn is defined as follows:
 
-// Only numbers 1 through 9 are used.
-// Each number is used at most once.
-// Return a list of all possible valid combinations.The list must not contain the same combination twice, and the combinations may be returned in any order.
+// T0 = 0, T1 = 1, T2 = 1, and Tn + 3 = Tn + Tn + 1 + Tn + 2 for n >= 0.
+
+// Given n, return the value of Tn.
 
 
 
 //     Example 1:
 
-// Input: k = 3, n = 7
-// Output: [[1, 2, 4]]
+// Input: n = 4
+// Output: 4
 // Explanation:
-// 1 + 2 + 4 = 7
-// There are no other valid combinations.
-//     Example 2:
+// T_3 = 0 + 1 + 1 = 2
+// T_4 = 1 + 1 + 2 = 4
+// Example 2:
 
-// Input: k = 3, n = 9
-// Output: [[1, 2, 6], [1, 3, 5], [2, 3, 4]]
-// Explanation:
-// 1 + 2 + 6 = 9
-// 1 + 3 + 5 = 9
-// 2 + 3 + 4 = 9
-// There are no other valid combinations.
-//     Example 3:
+// Input: n = 25
+// Output: 1389537
+function tribonacci(n) {
+    if (n === 0) return 0;
+    if (n === 1 || n === 2) return 1;
 
-// Input: k = 4, n = 1
-// Output: []
-// Explanation: There are no valid combinations.
-//     Using 4 different numbers in the range[1, 9], the smallest sum we can get is 1 + 2 + 3 + 4 = 10 and since 10 > 1, there are no valid combination.
-const combinationSum3 = function (k, n) {
-    const result = [];
-    const backtrack = (start, k, n, path) => {
-        if (k === 0 && n === 0) {
-            result.push([...path]);
-            return;
-        }
-        if (k === 0 || n === 0) {
-            return;
-        }
-        for (let i = start; i <= 9; i++) {
-            path.push(i);
-            backtrack(i + 1, k - 1, n - i, path);
-            path.pop();
-        }
-    };
+    let dp = new Array(n + 1);
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
 
-    backtrack(1, k, n, []);
-    return result;
-};
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    }
+
+    return dp[n];
+}
