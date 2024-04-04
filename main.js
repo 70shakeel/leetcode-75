aeb7b9f3e8b31c5ed019c47ba050c0b2c6d46abe
Shakeel <70shakeel@gmail.com>
2024-04-04 12:00:51 +0500

    Dynamic Programming: Domino and Tromino Tiling

--- main.js
+++ main.js
@@ -1,39 +1,36 @@
-// House Robber
+// Domino and Tromino Tiling
 // Medium
 // Topics
 // Companies
-// You are a professional robber planning to rob houses along a street.Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+// You have two types of tiles: a 2 x 1 domino shape and a tromino shape.You may rotate these shapes.
 
-// Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+// Given an integer n, return the number of ways to tile an 2 x n board.Since the answer may be very large, return it modulo 109 + 7.
+
+// In a tiling, every square must be covered by a tile.Two tilings are different if and only if there are two 4 - directionally adjacent cells on the board such that exactly one of the tilings has both squares occupied by a tile.
 
 
 
 //     Example 1:
 
-// Input: nums = [1, 2, 3, 1]
-// Output: 4
-// Explanation: Rob house 1(money = 1) and then rob house 3(money = 3).
-// Total amount you can rob = 1 + 3 = 4.
-// Example 2:
 
-// Input: nums = [2, 7, 9, 3, 1]
-// Output: 12
-// Explanation: Rob house 1(money = 2), rob house 3(money = 9) and rob house 5(money = 1).
-// Total amount you can rob = 2 + 9 + 1 = 12.
-function rob(nums) {
-    if (nums.length === 0) return 0;
-    if (nums.length === 1) return nums[0];
+// Input: n = 3
+// Output: 5
+// Explanation: The five different ways are show above.
+//     Example 2:
 
-    // Initialize an array to store the maximum money that can be robbed up to the ith house
-    const dp = new Array(nums.length);
-    dp[0] = nums[0];
-    dp[1] = Math.max(nums[0], nums[1]);
+// Input: n = 1
+// Output: 1
+function numTilings(n) {
+    const MOD = 1000000007;
+    const dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
 
-    // Calculate the maximum money that can be robbed up to the ith house
-    for (let i = 2; i < nums.length; i++) {
-        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+    for (let i = 3; i <= n; i++) {
+        dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD;
     }
 
-    // Return the maximum money that can be robbed from all houses
-    return dp[nums.length - 1];
+    return dp[n];
 }
