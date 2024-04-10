84426170a0edf903e2422d1169d0832a5a3ab2a3
Shakeel <70shakeel@gmail.com>
2024-04-10 10:40:53 +0500

    Bit Manipulation: Single Number

--- main.js
+++ main.js
@@ -1,35 +1,38 @@
-// Counting Bits
+// Single Number
 // Easy
 // Topics
 // Companies
-// Hint
-// Given an integer n, return an array ans of length n + 1 such that for each i(0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+// Given a non - empty array of integers nums, every element appears twice except for one.Find that single one.
+
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
 
 
 
-// Example 1:
+//     Example 1:
 
-// Input: n = 2
-// Output: [0, 1, 1]
-// Explanation:
-// 0 -- > 0
-// 1 -- > 1
-// 2 -- > 10
+// Input: nums = [2, 2, 1]
+// Output: 1
 // Example 2:
 
-// Input: n = 5
-// Output: [0, 1, 1, 2, 1, 2]
-// Explanation:
-// 0 -- > 0
-// 1 -- > 1
-// 2 -- > 10
-// 3 -- > 11
-// 4 -- > 100
-// 5 -- > 101
-function countBits(n) {
-    const ans = new Array(n + 1).fill(0);
-    for (let i = 1; i <= n; i++) {
-        ans[i] = ans[i >> 1] + (i & 1);
+// Input: nums = [4, 1, 2, 1, 2]
+// Output: 4
+// Example 3:
+
+// Input: nums = [1]
+// Output: 1
+
+
+// Constraints:
+
+// 1 <= nums.length <= 3 * 104
+//     - 3 * 104 <= nums[i] <= 3 * 104
+// Each element in the array appears twice except for one element which appears only once.
+function singleNumber(nums) {
+    let result = 0;
+
+    for (let num of nums) {
+        result ^= num;
     }
-    return ans;
+
+    return result;
 }
